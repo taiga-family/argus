77e1e9416f0c0e9d4edc9ed2a1f50ff25467cc10
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2021-09-03 10:06:31 +0300

    feat: update bot configs file name => `bot-configs.toml`

--- src/constants/bot-configs.constants.ts
+++ src/constants/bot-configs.constants.ts
@@ -1,6 +1,6 @@
 import {IBotConfigs} from '../types';
 
-export const BOT_CONFIGS_FILE_NAME = 'argus-configs.toml';
+export const BOT_CONFIGS_FILE_NAME = 'bot-configs.toml';
 
 export const DEFAULT_BOT_CONFIGS: Required<IBotConfigs> = {
     workflowWithTests: ['.*screenshot.*'],
